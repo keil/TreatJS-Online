153c59e4c147764a774efa6d5703b780250410dd
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-09-17 14:12:42 +0000

    add new TreatJS Version
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJSOnline/trunk@24782 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ treatjs_1.2.8/treat.convenience.js
@@ -0,0 +1,172 @@
+/*
+ * TreatJS: Higher-Order Contracts for JavaScript 
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ *
+ * Copyright (c) 2014, Proglang, University of Freiburg.
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ * All rights reserved.
+ *
+ * Released under the MIT license
+ * http://proglang.informatik.uni-freiburg.de/treatjs/license
+ *
+ * Author Matthias Keil
+ * http://www.informatik.uni-freiburg.de/~keilr/
+ */
+(function(_) {
+
+  var error = _.error;
+  var violation = _.violation;
+  var blame = _.blame;
+
+  var Contract = _.Core.Contract;
+  var Constructor = _.Core.Constructor;
+
+  var ContractConstructor = _.Constructor;
+
+  var BaseContract = _.BaseContract;
+
+  var FunctionContract = _.FunctionContract;
+  var MethodContract = _.MethodContract;
+  var DependentContract = _.DependentContract;
+  var ObjectContract = _.ObjectContract;
+
+  var WithContract = _.With;
+
+  var AndContract = _.And;
+  var OrContract = _.Or;
+  var NotContract = _.Not;
+
+  var Map = _.Map.Map;
+  var StringMap = _.Map.StringMap;
+  var Mapping = _.Map.Mapping;
+  var RegExpMap = _.Map.RegExpMap;
+
+  var Blank = new BaseContract(function(){return true;}, "-");
+  var BlankC = new ContractConstructor(function(){return Blank;});
+
+  //  ___  _     _        _    ___         _               _   
+  // / _ \| |__ (_)___ __| |_ / __|___ _ _| |_ _ _ __ _ __| |_ 
+  //| (_) | '_ \| / -_) _|  _| (__/ _ \ ' \  _| '_/ _` / _|  _|
+  // \___/|_.__// \___\__|\__|\___\___/_||_\__|_| \__,_\__|\__|
+  //          |__/                                             
+
+  function AdvancedObjectContract(mapping, strict) {
+    if(!(this instanceof AdvancedObjectContract)) return new AdvancedObjectContract(mapping, strict);
+
+    if(mapping instanceof Array) {
+      ObjectContract.call(this, StringMap(mapping, strict)); 
+    } else if(mapping instanceof Object) {
+      ObjectContract.call(this, StringMap(mapping, strict)); 
+    } else {
+      ObjectContract.call(this, StringMap({}, strict));
+    }
+
+  }
+  AdvancedObjectContract.prototype = new ObjectContract(new Map());
+
+  // ___          _   _   _          ___         _               _   
+  //| __|  _ _ _ | |_| |_(_)___ _ _ / __|___ _ _| |_ _ _ __ _ __| |_ 
+  //| _| || | ' \| / /  _| / _ \ ' \ (__/ _ \ ' \  _| '_/ _` / _|  _|
+  //|_| \_,_|_||_|_\_\\__|_\___/_||_\___\___/_||_\__|_| \__,_\__|\__|
+
+  function AdvancedFunctionContract(domain, range, strict) {
+    if(!(this instanceof AdvancedFunctionContract)) return new AdvancedFunctionContract(domain, range, strict);
+
+    if(!(range instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+
+    if(domain instanceof Array) {
+      FunctionContract.call(this, AdvancedObjectContract(domain, strict), range);
+    } else if(domain instanceof Object) {
+      FunctionContract.call(this, AdvancedObjectContract(domain, strict), range);
+    } else error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+  }
+  AdvancedFunctionContract.prototype = new FunctionContract(Blank, Blank);
+
+  function SimpleFunctionContract() {
+    if(!(this instanceof SimpleFunctionContract)) {
+      var base = Object.create(SimpleFunctionContract.prototype);
+      SimpleFunctionContract.apply(base, arguments);
+      return base;
+    }
+    if(arguments.length < 2) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+
+    var domain = [];
+    for(var i=0; i<arguments.length;i++) domain[i]=arguments[i];
+
+    var range = domain.pop();
+
+    FunctionContract.call(this, AdvancedObjectContract(domain, false), range);
+  }
+  SimpleFunctionContract.prototype = new FunctionContract(Blank, Blank);
+
+  // __  __     _   _            _  ___         _               _   
+  //|  \/  |___| |_| |_  ___  __| |/ __|___ _ _| |_ _ _ __ _ __| |_ 
+  //| |\/| / -_)  _| ' \/ _ \/ _` | (__/ _ \ ' \  _| '_/ _` / _|  _|
+  //|_|  |_\___|\__|_||_\___/\__,_|\___\___/_||_\__|_| \__,_\__|\__|
+
+  function AdvancedMethodContract(domain, range, context, strict) {
+    if(!(this instanceof AdvancedMethodContract)) return new AdvancedMethodContract(domain, range, context, strict);
+
+    if(!(range instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+    if(!(context instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+
+    if(domain instanceof Array) {
+      MethodContract.call(this, AdvancedObjectContract(domain, strict), range, context);
+    } else if(domain instanceof Object) {
+      MethodContract.call(this, AdvancedObjectContract(domain, strict), range, context);
+    } else error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+  }
+  AdvancedMethodContract.prototype = new MethodContract(Blank, Blank, Blank);
+
+  function SimpleMethodContract() {
+    if(!(this instanceof SimpleMethodContract)) {
+      var base = Object.create(SimpleMethodContract.prototype);
+      SimpleMethodContract.apply(base, arguments);
+      return base;
+    }
+    if(arguments.length < 3) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+
+    var domain = [];
+    for(var i=0; i<arguments.length;i++) domain[i]=arguments[i];
+
+    var context = domain.pop();
+    var range = domain.pop();
+
+    MethodContract.call(this, AdvancedObjectContract(domain, false), range, context);
+  }
+  SimpleMethodContract.prototype = new MethodContract(Blank, Blank, Blank);
+
+  // ___                        _         _    ___         _               _   
+  //|   \ ___ _ __  ___ _ _  __| |___ _ _| |_ / __|___ _ _| |_ _ _ __ _ __| |_ 
+  //| |) / -_) '_ \/ -_) ' \/ _` / -_) ' \  _| (__/ _ \ ' \  _| '_/ _` / _|  _|
+  //|___/\___| .__/\___|_||_\__,_\___|_||_\__|\___\___/_||_\__|_| \__,_\__|\__|
+  //         |_|                                                               
+
+  function SimpleDependentContract(name, contract) {
+    if(!(this instanceof SimpleDependentContract)) return new SimpleDependentContract(name, contract);
+
+    // constructor function
+    function ctor() {
+      var binding = {};
+      binding[name]=arguments;
+      return _.With(binding, contract);
+    }
+    DependentContract.call(this, ContractConstructor(ctor, {name:name, contract:contract}));
+  }
+  SimpleDependentContract.prototype = new DependentContract(BlankC);
+
+  /**
+   * Convenience Contracts
+   */
+
+  __define("AdvancedObjectContract", AdvancedObjectContract, _);
+
+  __define("AdvancedFunctionContract", AdvancedFunctionContract, _);
+  __define("SimpleFunctionContract", SimpleFunctionContract, _);
+
+  __define("AdvancedMethodContract", AdvancedMethodContract, _);
+  __define("SimpleMethodContract", SimpleMethodContract, _);
+
+  __define("SimpleDependentContract", SimpleDependentContract, _);
+
+})(TreatJS);
---
+++ treatjs_1.2.8/treat.js
@@ -0,0 +1,205 @@
+/*
+ * TreatJS: Higher-Order Contracts for JavaScript 
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ *
+ * Copyright (c) 2014, Proglang, University of Freiburg.
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ * All rights reserved.
+ *
+ * Released under the MIT license
+ * http://proglang.informatik.uni-freiburg.de/treatjs/license
+ *
+ * Author Matthias Keil
+ * http://www.informatik.uni-freiburg.de/~keilr/
+ */
+
+// _____             _      _ ___ 
+//|_   _| _ ___ __ _| |_ _ | / __|
+//  | || '_/ -_) _` |  _| || \__ \
+//  |_||_| \___\__,_|\__|\__/|___/
+//                                
+//  ___ _     _          _    ___  _     _        _   
+// / __| |___| |__  __ _| |  / _ \| |__ (_)___ __| |_ 
+//| (_ | / _ \ '_ \/ _` | | | (_) | '_ \| / -_) _|  _|
+// \___|_\___/_.__/\__,_|_|  \___/|_.__// \___\__|\__|
+//                                    |__/            
+function TreatJS(configuration) {
+  if(!(this instanceof TreatJS)) return new TreatJS(configuration);
+
+  this.toString = (function() { return '[[TreatJS]]'; });
+
+  this.configure = function(configuration) {
+    for(setting in configuration) {
+      this.Config[setting] = configuration[setting];
+    } 
+  };
+
+  this.verbose = function(configuration) {
+    for(setting in configuration) {
+      this.Config.Verbose[setting] = configuration[setting];
+    } 
+  }
+
+  this.build = function() {
+    var _ = {};
+
+    // assert
+    __define("construct", this.construct, _);
+    __define("assert", this.assert, _);
+
+    // contract
+    __define("Base", this.BaseContract, _);
+
+    __define("Function", this.FunctionContract, _);
+    __define("Method", this.MethodContract, _);
+    __define("Dependent", this.DependentContract, _);
+
+    __define("Object", this.ObjectContract, _);
+
+    __define("With", this.With, _);
+
+    __define("Union", this.Union, _);
+    __define("Intersection", this.Intersection, _);
+    __define("Negation", this.Negation, _);
+
+    __define("And", this.And, _);
+    __define("Or", this.Or, _);
+    __define("Not", this.Not, _);
+
+    __define("Constructor", this.Constructor, _);
+
+    // convinience
+    __define("AObject", this.AdvancedObjectContract, _);
+
+    __define("AFunction", this.AdvancedFunctionContract, _);
+    __define("SFunction", this.SimpleFunctionContract, _);
+
+    __define("AMethod", this.AdvancedMethodContract, _);
+    __define("SMethod", this.SimpleMethodContract, _);
+
+    __define("SDependent", this.SimpleDependentContract, _);
+
+    // util
+    __define("StringMap", this.Map.StringMap, _);
+    __define("Mapping", this.Map.Mapping, _);
+    __define("RegExpMap", this.Map.RegExpMap, _);
+
+    return _;
+  }
+
+  this.export = function(target, prefix, suffix) {
+    prefix = (prefix===undefined) ? "__" : prefix;
+    suffix = (suffix===undefined) ? "__" : suffix;
+
+    // assert
+    __define(prefix + "construct" + suffix, this.construct, target);
+    __define(prefix + "assert" + suffix, this.assert, target);
+
+    // contract
+    __define(prefix + "Base" + suffix, this.BaseContract, target);
+
+    __define(prefix + "Function" + suffix, this.FunctionContract, target);
+    __define(prefix + "Method" + suffix, this.MethodContract, target);
+    __define(prefix + "Dependent" + suffix, this.DependentContract, target);
+
+    __define(prefix + "Object" + suffix, this.ObjectContract, target);
+
+    __define(prefix + "With" + suffix, this.With, target);
+
+    __define(prefix + "Union" + suffix, this.Union, target);
+    __define(prefix + "Intersection" + suffix, this.Intersection, target);
+    __define(prefix + "Negation" + suffix, this.Negation, target);
+
+    __define(prefix + "And" + suffix, this.And, target);
+    __define(prefix + "Or" + suffix, this.Or, target);
+    __define(prefix + "Not" + suffix, this.Not, target);
+
+    __define(prefix + "Constructor" + suffix, this.Constructor, target);
+
+    // convinience
+    __define(prefix + "AObject" + suffix, this.AdvancedObjectContract, target);
+
+    __define(prefix + "AFunction" + suffix, this.AdvancedFunctionContract, target);
+    __define(prefix + "SFunction" + suffix, this.SimpleFunctionContract, target);
+
+    __define(prefix + "AMethod" + suffix, this.AdvancedMethodContract, target);
+    __define(prefix + "SMethod" + suffix, this.SimpleMethodContract, target);
+
+    __define(prefix + "SDependent" + suffix, this.SimpleDependentContract, target);
+
+    // util
+    __define(prefix + "StringMap" + suffix, this.Map.StringMap, target);
+    __define(prefix + "Mapping" + suffix, this.Map.Mapping, target);
+    __define(prefix + "RegExpMap" + suffix, this.Map.RegExpMap, target);
+  }
+
+  function makeManual() {
+    var Manual = [];
+
+    function __include (name, entry, classid) {
+      var id = (classid || "Miscellaneous");
+      if (Manual[id] == undefined) Manual[id] = [];
+      Manual[id][name] = entry;
+    }
+
+    Manual.toString = function () {
+      var str = "";
+      for (var cl in Manual) {
+        str += "* " + cl + "\n\n";
+        for (var en in Manual[cl]) {
+          str += "** " + en + "\n";
+        }
+        str += "\n";
+      }
+      return str;
+    }
+
+    // assert
+    __include("assert", this.assert, "Methods");
+    __include("construct", this.construct, "Methods");
+
+    // contract
+    __include("Base", this.BaseContract, "Core - Contrcts");
+
+    __include("Function", this.FunctionContract, "Core - Contrcts");
+    __include("Method", this.MethodContract, "Core - Contrcts");
+    __include("Dependent", this.DependentContract, "Core - Contrcts");
+
+    __include("Object", this.ObjectContract, "Core - Contrcts");
+
+    __include("With", this.With, "Core - Contrcts");
+
+    __include("Union", this.Union, "Core - Contrcts");
+    __include("Intersection", this.Intersection, "Core - Contrcts");
+    __include("Negation", this.Negation, "Core - Contrcts");
+
+    __include("And", this.And, "Core - Contrcts");
+    __include("Or", this.Or, "Core - Contrcts");
+    __include("Not", this.Not, "Core - Contrcts");
+
+    __include("Constructor", this.Constructor, "Contract Constructor");
+
+    // convinience
+    __include("AObject", this.AdvancedObjectContract, "Convenience - Contrcts");
+
+    __include("AFunction", this.AdvancedFunctionContract, "Convenience - Contrcts");
+    __include("SFunction", this.SimpleFunctionContract, "Convenience - Contrcts");
+
+    __include("AMethod", this.AdvancedMethodContract, "Convenience - Contrcts");
+    __include("SMethod", this.SimpleMethodContract, "Convenience - Contrcts");
+
+    __include("SDependent", this.SimpleDependentContract, "Convenience - Contrcts");
+
+    // util
+    __include("StringMap", this.Map.StringMap, "Maps");
+    __include("RegExpMap", this.Map.RegExpMap, "Maps");
+    __include("Mapping", this.Map.Mapping, "Maps");
+
+    return Manual;
+  }
+
+  this.Manual = makeManual();
+}
+
+// TreatJS
+var TreatJS = new TreatJS();
